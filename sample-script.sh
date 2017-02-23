20b3a5d0755b66164ebc51dc65817258219e36e5
Daniel Teske <daniel.teske@meteogroup.com>
2017-02-23 14:51:14 +0100

    added java, copy demolocation while image build use real grb

---
+++ sample-script.sh
@@ -0,0 +1,55 @@
+#! /bin/bash
+
+#
+# SCRIPT PREPARATION
+#
+export GISRC=$HOME/.grassrc7
+
+# path to GRASS binaries and libraries:
+export GISBASE=/usr/lib/grass72
+
+export PATH=$PATH:$GISBASE/bin:$GISBASE/scripts
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$GISBASE/lib
+
+# use process ID (PID) as lock file number:
+export GIS_LOCK=$$
+
+# settings for graphical output to PNG file (optional)
+export GRASS_PNGFILE=/tmp/grass6output.png
+export GRASS_TRUECOLOR=TRUE
+export GRASS_WIDTH=900
+export GRASS_HEIGHT=1200
+export GRASS_PNG_COMPRESSION=1
+export GRASS_MESSAGE_FORMAT=plain
+
+#
+# SCRIPT
+#
+
+# debug GRASS and GDAL version
+g.version
+ogrinfo --version
+
+gdal_contour -i 5 -off 0 -a value sample-data/sample-pressure.grb sample-data/contours.shp
+
+#1. import raster
+v.in.ogr input=sample-data/contours.shp output=contours
+
+#3. smooth corners with Chaiken's algorithm to get smoother curves
+v.generalize --overwrite method=sliding_averaging threshold=1.0 input=contours output=isolines_all
+
+v.generalize --overwrite method=sliding_averaging threshold=1.0 input=isolines_all output=isolines_all2
+
+v.generalize --overwrite method=douglas threshold=0.005 input=isolines_all2 output=isolines_all2_sim
+
+v.out.ogr --overwrite type=line input=isolines_all2_sim output=sample-data/isolines_all2_sim.shp
+
+#4. remove too small isolines
+ogr2ogr -f "GeoJSON" -lco COORDINATE_PRECISION=2 -dialect sqlite -sql "select * from isolines_all2_sim where ST_LENGTH(GEOMETRY) >= 1" sample-data/isolines_sim.json sample-data/isolines_all2_sim.shp
+
+rm -rf sample-data/*.dbf
+rm -rf sample-data/*.prj
+rm -rf sample-data/*.shp
+rm -rf sample-data/*.shx
+
+echo "DONE."
